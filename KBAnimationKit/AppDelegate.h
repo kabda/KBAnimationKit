b99a28e7ebf45abdc2295e5aad6f323f6f4108ef
fanyuandong <fanyuandong@didichuxing.com>
2016-04-22 14:48:23 +0800

    new proj

---
+++ KBAnimationKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KBAnimationKit
+//
+//  Created by 樊远东 on 4/22/16.
+//  Copyright © 2016 樊远东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KBAnimationKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KBAnimationKit
+//
+//  Created by 樊远东 on 4/22/16.
+//  Copyright © 2016 樊远东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
